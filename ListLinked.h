806a18b655e09a7c1a6d8142ef407f7cba8ef04d
Iria Lopez Falces <ilopfal@upv.edu.es>
2024-12-30 20:42:45 +0100

    Añadida implementación de la clase ListLinked

--- ListLinked.h
+++ ListLinked.h
@@ -11,14 +11,12 @@ using namespace std;
 template <typename T>
 class ListLinked : public List<T> {
 private:
-    Node<T>* first; // Puntero al primer nodo de la lista
-    int n; // Número de elementos en la lista
+    Node<T>* first;
+    int n;
 
 public:
-    // Constructor sin argumentos
     ListLinked() : first(nullptr), n(0) {}
 
-    // Destructor
     ~ListLinked() {
         Node<T>* current = first;
         while (current != nullptr) {
@@ -28,23 +26,21 @@ public:
         }
     }
 
-    // Sobrecarga del operador []
-    T operator[](int pos) override {
+    T operator[](int pos) {
         if (pos < 0 || pos >= n) {
             throw out_of_range("Index out of range");
         }
-        Node<T>* temp = first;
+        Node<T>* current = first;
         for (int i = 0; i < pos; ++i) {
-            temp = temp->next;
+            current = current->next;
         }
-        return temp->data;
+        return current->data;
     }
 
-    // Sobrecarga del operador de salida para imprimir la lista
     friend ostream& operator<<(ostream &out, const ListLinked<T> &list) {
         Node<T>* current = list.first;
-        out << "[";
-        while (current != nullptr) {
+        out << "List -> [";
+        while (current) {
             out << current->data;
             if (current->next != nullptr) {
                 out << ", ";
@@ -55,6 +51,88 @@ public:
         return out;
     }
 
+    void insert(int pos, T e) {
+        if (pos < 0 || pos > n) {
+            throw out_of_range("Index out of range");
+        }
+        Node<T>* newNode = new Node<T>(e);
+        if (pos == 0) {
+            newNode->next = first;
+            first = newNode;
+        } else {
+            Node<T>* current = first;
+            for (int i = 1; i < pos; i++) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+        n++;
+    }
+
+    void append(T e) {
+        insert(n, e);
+    }
+
+    void prepend(T e) {
+        insert(0, e);
+    }
+
+    T remove(int pos) {
+        if (pos < 0 || pos >= n) {
+            throw out_of_range("Index out of range");
+        }
+        Node<T>* current = first;
+        T data;
+        if (pos == 0) {
+            first = first->next;
+            data = current->data;
+            delete current;
+        } else {
+            Node<T>* prev = nullptr;
+            for (int i = 0; i < pos; i++) {
+                prev = current;
+                current = current->next;
+            }
+            prev->next = current->next;
+            data = current->data;
+            delete current;
+        }
+        n--;
+        return data;
+    }
+
+    T get(int pos) const {
+        if (pos < 0 || pos >= n) {
+            throw out_of_range("Index out of range");
+        }
+        Node<T>* current = first;
+        for (int i = 0; i < pos; i++) {
+            current = current->next;
+        }
+        return current->data;
+    }
+
+    int search(T e) const {
+        Node<T>* current = first;
+        int index = 0;
+        while (current != nullptr) {
+            if (current->data == e) {
+                return index;
+            }
+            current = current->next;
+            index++;
+        }
+        return -1; // Not found
+    }
+
+    bool empty() const override {
+        return n == 0;
+    }
+
+    int size() const override {
+        return n;
+    }
 };
 
 #endif // LISTLINKED_H
